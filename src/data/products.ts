f89ba7f50b6ec0a897d7eadd2cb8808c58a84ffc
jonavon7 <ana@MacBook-Air.local>
2025-05-23 14:29:25 +0200

    data

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,6 +1,6 @@
 import { Product } from "@/models/Product";
 
-const exampleFin = 'https://images.pexels.com/photos/4603876/pexels-photo-4603876.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load'
+const exampleFin = 'https://images.pexels.com/photos/4603876/pexels-photo-4603876.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load';
 
 export const products: Product[] = [
     {
@@ -13,6 +13,66 @@ export const products: Product[] = [
         id: '2',
         name: 'Cotton Oversized Tee',
         price: '$29',
+        image: exampleFin,
+    },
+    {
+        id: '3',
+        name: 'Slim Fit Chinos',
+        price: '$49',
+        image: exampleFin,
+    },
+    {
+        id: '4',
+        name: 'Leather Chelsea Boots',
+        price: '$129',
+        image: exampleFin,
+    },
+    {
+        id: '5',
+        name: 'Wool Blend Coat',
+        price: '$199',
+        image: exampleFin,
+    },
+    {
+        id: '6',
+        name: 'Casual Sneakers',
+        price: '$89',
+        image: exampleFin,
+    },
+    {
+        id: '7',
+        name: 'Striped Polo Shirt',
+        price: '$39',
+        image: exampleFin,
+    },
+    {
+        id: '8',
+        name: 'Knitted Scarf',
+        price: '$25',
+        image: exampleFin,
+    },
+    {
+        id: '9',
+        name: 'Canvas Backpack',
+        price: '$59',
+        image: exampleFin,
+    },
+    {
+        id: '10',
+        name: 'Aviator Sunglasses',
+        price: '$99',
+        image: exampleFin,
+    },
+    {
+        id: '11',
+        name: 'Graphic Hoodie',
+        price: '$69',
+        image: exampleFin,
+    },
+    {
+        id: '12',
+        name: 'Sports Watch',
+        price: '$149',
         image: exampleFin,
     },
 ];
